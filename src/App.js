3c5faeba7ad8a55618d95d97699ef9847e48143f
hakunamoni <hakunamoni@gmail.com>
2021-12-15 22:32:33 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react'; 
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
 function App() {
+  console.log(web3);
   return (
     <div className="App">
       <header className="App-header">
