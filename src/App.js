34cf3f0884cf6d3f685fabd514f9a1ef31df7ccd
hakunamoni <hakunamoni@gmail.com>
2021-12-16 22:27:41 +0800

    Add form elements - input, button

--- src/App.js
+++ src/App.js
@@ -6,28 +6,44 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  // constructor(props){
-  //   super(props);
-    
-  //   this.state = { manager: '' };
-  // }
-
-  // same functionality as the above construcotr
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: '',   // balance is not actually a number, it's actually an object, it's a number that is wrapped in the library called "bignumber.js"
+    value: ''     //this is similar to balance, but in this case, it's string owing to the text input is going to be empty string
   };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        
+        <hr />
+
+        <form>
+          <h4> Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+              value={this.state.value}
+              onChange={ event => this.setState({ value: event.target.value })} 
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );  
   }
