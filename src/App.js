51a17ab24d9016e07e959c254096b4eb26f1d846
hakunamoni <hakunamoni@gmail.com>
2021-12-16 23:31:49 +0800

    add message to show sending tx process

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ class App extends Component {
     manager: '',
     players: [],
     balance: '',
-    value: ''
+    value: '',
+    message: ''
   };
 
   async componentDidMount() {
-    // whenever making call() in javascript react world that uses the provider from web3, 
-    // don't have to make use of "from" property that specify which account is sending transaction
-    // that is true with call()
     const manager = await lottery.methods.manager().call();
     const players = await lottery.methods.getPlayers().call();
     const balance = await web3.eth.getBalance(lottery.options.address);
@@ -24,31 +22,20 @@ class App extends Component {
     this.setState({ manager, players, balance });
   }
 
-  // onSubmit() {
-  // }
-  // when make use of any event handler with react, worry about the context ("this") of the function
-  // traditionally, did such calling "<form onSubmit={this.onSubmit.bind(this)}>"
-  // with the version of "Babel" (that is running inside the create-react-app start pack), don't manually binding the function
-  // rather than using above syntax ("onSubmit() {}"), use the following syntax
-  // onSubmit = () => {};
-  // important thing, don't worry about using "this" inside of the function 
-  // the value of "this" will be automatically set to be equal to the component
   onSubmit = async (event) => {
-    // whenever onSubmit gets called, it will be called with an "event" object and that "event" represents the form submission
   
     event.preventDefault();
-    // make sure the form does not attempt to submit itself in the classic HTML
 
-    // send transaction
-    // for sending transaction, it's not true to not specify the "from" property
-    // it should retrieve list of accounts from the web3 object 
-    // and specify the account that's used to send money over to that particular function that exists on the contract
     const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...'});
     
     await lottery.methods.enter().send({
       from: accounts[0],
       value: web3.utils.toWei(this.state.value, 'ether')
     });
+
+    this.setState({ message: 'You have been entered!'});
   };
 
   render() {
@@ -74,6 +61,10 @@ class App extends Component {
           </div>
           <button>Enter</button>
         </form>
+
+        <hr />
+
+        <h1>{this.state.message}</h1>
       </div>
     );  
   }
